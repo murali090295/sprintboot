9694b83bf36a3691067b6e71705f7d2523ce9c6c
murali090295 <109014107+murali090295@users.noreply.github.com>
2022-07-14 15:31:31 +0530

    project files

---
+++ src/test/java/Devops/Test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package Devops.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
